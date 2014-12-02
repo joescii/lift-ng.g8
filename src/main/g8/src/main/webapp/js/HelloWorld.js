b4494155e80a539780e19ee9f87040017b7aeb8b
Joe Barnes <barnesjd@gmail.com>
2014-12-02 07:32:46 -0600

    The page load time in js is a function, not a field

--- src/main/g8/src/main/webapp/js/HelloWorld.js
+++ src/main/g8/src/main/webapp/js/HelloWorld.js
@@ -2,7 +2,7 @@ angular.module('HelloWorldApp', ['HelloServices']).
 
 controller('Hello', ['$scope', 'helloService', function($scope, hello) {
   $scope.svc = hello;
-  $scope.pageLoadTime = hello.pageLoadTime;
+  $scope.pageLoadTime = hello.pageLoadTime();
 
   $scope.checkTime = function() {
     hello.currentServerTime().then(function(time){
